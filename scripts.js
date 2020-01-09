6d0d95664f145046c6c77ddefe729c5e9530f499
alejandropyx <59699071+alejandropyx@users.noreply.github.com>
2020-01-09 10:26:12 -0500

    add files

---
+++ scripts.js
@@ -0,0 +1,51 @@
+
+fetch('DB.txtt')
+  .then(response => response.text())
+  .then(text => console.log(text))
+
+
+function selectClass(x) {
+   element = document.getElementById(x.id);
+	var x = document.getElementsByClassName("class");     
+	for (var i = 0; i < x.length; i++)
+	{
+		element2 = document.getElementById(x[i].id);
+		if (element2 .classList.contains("selectedClass")) {
+		element2.classList.remove("selectedClass");
+	}
+	}
+	 document.getElementById("modeContainer").style.display = "Inline";
+   
+	element.classList.add("selectedClass");
+};
+
+
+function meeting(x) {
+   element = document.getElementById(x.id);
+	var x = document.getElementsByClassName("type");     
+	for (var i = 0; i < x.length; i++)
+	{
+		element2 = document.getElementById(x[i].id);
+		if (element2 .classList.contains("selectedMode")) {
+		element2.classList.remove("selectedMode");
+	}
+	}
+	 document.getElementById("HoursContainer").style.display = "Inline";
+   
+	element.classList.add("selectedMode");
+};
+
+function hour(x) {
+   element = document.getElementById(x.id);
+	var x = document.getElementsByClassName("hour");     
+	for (var i = 0; i < x.length; i++)
+	{
+		element2 = document.getElementById(x[i].id);
+		if (element2 .classList.contains("selectedHour")) {
+		element2.classList.remove("selectedHour");
+	}
+	}   
+	element.classList.add("selectedHour");
+	$('.transform').toggleClass('transform-active');
+
+};
